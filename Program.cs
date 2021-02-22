cf8d4207a28c00647999ef584cfdadf58b12c4d7
zachary <zhuba@my.wctc.edu>
2021-02-22 15:07:53 -0600

    Real Final Version

--- Program.cs
+++ Program.cs
@@ -24,85 +24,92 @@ namespace MovieLibrary
                     Console.WriteLine("Not a valid choice!");
                 }
                 string file = "movies.csv";
-                
-                if(choice == 1)
-                {                  
-                    StreamReader sr = new StreamReader(file);
-                    while(!sr.EndOfStream)
-                    {
-                        string movieInfo = sr.ReadLine();
-                        string[] movieInfoParts = movieInfo.Split(",");
-                        string movieID = movieInfoParts[0];
-                        string movieTitle = movieInfoParts[1];
-                        string[] genres = movieInfoParts[2].Split("|");
-                        movieList.Add(movieTitle);
 
-                        Console.Write($"ID: {movieID}, Title: {movieTitle}, Genre(s): ");
-
-                        for(int i = 0; i < genres.Length; i++)
+                if(!File.Exists(file))
+                {
+                    logger.Error("File does not exist");
+                }
+                else
+                {
+                    if(choice == 1)
+                    {                  
+                        StreamReader sr = new StreamReader(file);
+                        while(!sr.EndOfStream)
                         {
-                            if(i < genres.Length - 1)
-                            {
-                                Console.Write(genres[i] + ", ");
-                            }
-                            else
-                            {
-                                Console.Write(genres[i]);
-                            }
-                        }
-                        Console.WriteLine("");
-                    }
-                    sr.Close();
-                } 
-                else if(choice == 2)
-                {                    
-                    if(System.IO.File.Exists(file))
-                    {
-                        StreamWriter sw = new StreamWriter(@"movies.csv", true);
+                            string movieInfo = sr.ReadLine();
+                            string[] movieInfoParts = movieInfo.Split(",");
+                            string movieID = movieInfoParts[0];
+                            string movieTitle = movieInfoParts[1];
+                            string[] genres = movieInfoParts[2].Split("|");
+                            movieList.Add(movieTitle);
 
-                        Console.WriteLine("Enter the id:");
-                        string id = Console.ReadLine();
+                            Console.Write($"ID: {movieID}, Title: {movieTitle}, Genre(s): ");
 
-                        Console.WriteLine("What is the title of the movie?");
-                        string movieTitle2 = Console.ReadLine();
-                        if(movieList.Contains(movieTitle2))
-                        {
-                            Console.WriteLine("Movie exists already");
-                        }
-                        else
-                        {
-                            var movieGenres = new ArrayList();
-                            Console.WriteLine("How many genres does this movie fit into? ");
-                            int numGenres;
-                            if(!int.TryParse(Console.ReadLine(), out numGenres))
+                            for(int i = 0; i < genres.Length; i++)
                             {
-                                Console.WriteLine("Not a number");
-                            }
-
-                            for(int i = 0; i < numGenres; i++)
-                            {
-                                Console.WriteLine("Please enter the name of the genre:");
-                                string addToGenres = Console.ReadLine();
-                                movieGenres.Add(addToGenres);
-                            }
-                            sw.WriteLine("");
-                            sw.Write($"{id},{movieTitle2},");
-
-                            for(int i = 0; i < movieGenres.Count; i++)
-                            {
-                                if(i < movieGenres.Count - 1)
+                                if(i < genres.Length - 1)
                                 {
-                                    sw.Write(movieGenres[i] + "|");
+                                    Console.Write(genres[i] + ", ");
                                 }
                                 else
                                 {
-                                    sw.Write(movieGenres[i]);
+                                    Console.Write(genres[i]);
                                 }
                             }
                             Console.WriteLine("");
-                            sw.Close();
                         }
+                        sr.Close();
                     } 
+                    else if(choice == 2)
+                    {                    
+                        if(System.IO.File.Exists(file))
+                        {
+                            StreamWriter sw = new StreamWriter(@"movies.csv", true);
+
+                            Console.WriteLine("Enter the id:");
+                            string id = Console.ReadLine();
+
+                            Console.WriteLine("What is the title of the movie?");
+                            string movieTitle2 = Console.ReadLine();
+                            if(movieList.Contains(movieTitle2))
+                            {
+                                Console.WriteLine("Movie exists already");
+                            }
+                            else
+                            {
+                                var movieGenres = new ArrayList();
+                                Console.WriteLine("How many genres does this movie fit into? ");
+                                int numGenres;
+                                if(!int.TryParse(Console.ReadLine(), out numGenres))
+                                {
+                                    Console.WriteLine("Not a number");
+                                }
+
+                                for(int i = 0; i < numGenres; i++)
+                                {
+                                    Console.WriteLine("Please enter the name of the genre:");
+                                    string addToGenres = Console.ReadLine();
+                                    movieGenres.Add(addToGenres);
+                                }
+                                sw.WriteLine("");
+                                sw.Write($"{id},{movieTitle2},");
+
+                                for(int i = 0; i < movieGenres.Count; i++)
+                                {
+                                    if(i < movieGenres.Count - 1)
+                                    {
+                                        sw.Write(movieGenres[i] + "|");
+                                    }
+                                    else
+                                    {
+                                        sw.Write(movieGenres[i]);
+                                    }
+                                }
+                                Console.WriteLine("");
+                                sw.Close();
+                            }
+                        } 
+                    }
                 }
             }while(choice != 0);
         }
